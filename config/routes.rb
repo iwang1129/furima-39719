08797270a45f69463f41166422ade67f38372411
iwang1129 <tatekuni469@icloud.com>
2023-08-29 17:46:17 +0900

    修正５

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :items, only: :index
   # Defines the root path route ("/")
   root "items#index"
   # トップページを表示する
