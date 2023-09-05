4cb1eacf4cc8cd9f7064ea24334a1f696df01ce8
iwang1129 <tatekuni469@icloud.com>
2023-09-05 22:17:59 +0900

    Revert "商品詳細表示機能"
    This reverts commit 55c00976dbce41d45bf118f7e9e63992712ddaf4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,14 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
-
-
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :explanation, :category_id, :situation_id, :load_id, :region_id, :shipping_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :situation_id, :load_id, :region_id, :shipping_id,
+                                 :price).merge(user_id: current_user.id)
   end
 end
